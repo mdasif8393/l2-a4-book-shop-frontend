30322e31fa4094204eb4bb3ade850e983ed989a9
mdasif8393 <md.asif.8393@gmail.com>
2025-01-27 17:01:05 +0600

    add redux store and auth slice and auth reducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.tsx";
+import "./index.css";
+import { persistor, store } from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate persistor={persistor}></PersistGate>
+      <App />
+    </Provider>
+  </StrictMode>
+);
